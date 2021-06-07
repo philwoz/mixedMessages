788719d78f8ce92b795bf72e885a0380f9e4e47a
philwoz <61164019+philwoz@users.noreply.github.com>
2021-06-07 16:23:11 +0100

    added generate quote function

--- src/app.js
+++ src/app.js
@@ -3,15 +3,22 @@ const data = require('./quotes')
 
 //get a random num between 0 and length of arr
 const ranNum = (arr) => {
-    return Math.floor(Math.random() * arr.length)
+    return Math.floor(Math.random() * arr.length);
 
 }
 
 const getRanQuote = (arr) => {
-    return arr[ranNum(arr)]
+    return arr[ranNum(arr)];
 }
-let a = [1,2]
-let arr = data.lyrics;
-console.log(getRanQuote(arr))
 
-console.log(ranNum(arr))
+const generateQuote = (arr) => {
+    console.log("*****OASIS QUOTE Machine *******")
+    console.log(`>>>> Noel Quote : ${getRanQuote(arr.noel)}`)
+    console.log("*****OASIS QUOTE Machine")
+    console.log(`>>>>Liam   Quote : ${getRanQuote(arr.liam)}`)
+    console.log("*****OASIS QUOTE Machine")
+    console.log(`>>>>Lyric : ${getRanQuote(arr.lyrics)}`)
+}
+ 
+generateQuote(data)
+
